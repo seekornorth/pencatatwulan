1d30fd8cc9511e22baffaa54e494cf3ea4c37a2b
Seekor North <northsevent012@gmail.com>
2026-06-03 18:04:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnThBhBReeyp6kTeP4nlLfQsWKveOgB0MfMc5ddMB7kU_BuC-A7Q8ophunV-YnGeKkbmne8m7jqK6Jb5WUBty8YOzizIINEtqRu-2Rwb351plPbaNFCYJVlmQh-nmffbuRxW12s7DRSY0PeYtLHbxRgVtYLsro-Yz-RQeWQz71U2A_qDLMJlpiM7F4jE72kpSpO7NnjFJ6vixW3rD2FYoazqeSdH-hebknulf47On691PdI2GGkkDs4r-go4DWh9dbT-8_fe0EjlNQLYvE_aF2d4n6L5fQ&lib=M04E52ro3yPgQd2Quri6tj3tq80Kj4Y18";
+const API_URL = "https://script.google.com/macros/s/AKfycbw0jBHLLz42MCehM9ksuP6WC7VJyCmcM2U7D8KeGx0y8D1ksM7HbiVBuF5P7nOHoD_s/exec";
 
 const form = document.getElementById("formTransaksi");
 const tbody = document.getElementById("tbody");
